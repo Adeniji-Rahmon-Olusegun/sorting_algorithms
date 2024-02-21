4aca925b52e9806dbb208bd9349a790fc8f85bbd
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-02-21 08:55:10 -0800

    0-bubble_sort.c file added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an unsorted array using
+ * buble sort algorithm
+ *
+ * @array: unsorted array
+ * @size: length of the array
+ *
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int idx, jdex, flag;
+
+	for (idx = 0; idx < size - 1; idx++)
+	{
+		flag = 0;
+
+		for (jdex = 0; jdex < size - 1 - idx; jdex++)
+		{
+			if (array[jdex] > array[jdex + 1])
+			{
+				temp = array[jdex];
+				array[jdex] = array[jdex + 1];
+				array[jdex + 1] = temp;
+
+				print_array(array, size);
+
+				flag = 1;
+			}
+		}
+
+		if (flag == 0)
+			break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+#0x1B. C - Sorting algorithms & Big O
+
+##General
+
+- Allowed editors: vi, vim, emacs
+
+- All your files are compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+
+- All files end with a new line
+
+- All code are Betty style compliant. It is checked using betty-style.pl and betty-doc.pl
+
+- All prototypes of all functions should are included in the header file called sort.h
+
+- A list/array would not be sorted if its size is less than 2.
